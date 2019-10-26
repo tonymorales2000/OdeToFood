521f7d7507949d08c0d5361b4704d8b5c4c0b022
tonymorales <tonymorales2000@hotmail.com>
2019-10-25 21:20:11 -0600

    Initial Commit

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant{ Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.None},
+                new Restaurant{ Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) ||  r.Name.ToLower().Contains(name.ToLower())
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
