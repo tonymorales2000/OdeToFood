96f5aadb543d06c52b8c56c640d6664c854f8ff0
tonymorales2000 <56054947+tonymorales2000@users.noreply.github.com>
2019-10-27 20:49:22 -0600

    Scaffolding CRUD and parital ValidationScript

---
+++ OdeToFood/OdeToFood/Pages/RestaurantScaf/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.RestaurantScaf
+{
+    public class CreateModel : PageModel
+    {
+        private readonly OdeToFood.Data.OdeToFoodDbContext _context;
+        private readonly IHtmlHelper htmlHelper;
+
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public CreateModel(OdeToFood.Data.OdeToFoodDbContext context, IHtmlHelper htmlHelper)
+        {
+            _context = context;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet()
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            return Page();
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Restaurants.Add(Restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Pages/RestaurantScaf/Edit.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.RestaurantScaf
+{
+    public class EditModel : PageModel
+    {
+        private readonly OdeToFood.Data.OdeToFoodDbContext _context;
+        private readonly IHtmlHelper htmlHelper;
+
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(OdeToFood.Data.OdeToFoodDbContext context, IHtmlHelper htmlHelper)
+        {
+            _context = context;
+            this.htmlHelper = htmlHelper;
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Restaurant = await _context.Restaurants.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Restaurant == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Restaurant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RestaurantExists(Restaurant.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool RestaurantExists(int id)
+        {
+            return _context.Restaurants.Any(e => e.Id == id);
+        }
+    }
+}
